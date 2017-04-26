417909e2a27dde5271bcd67cd42ddf50268a4604
Matt Drapchaty <Drapchaty@comcast.net>
2017-04-25 21:30:00 -0600

    links and APIS all working

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function show(target, div){
+				document.getElementById(target).style.display = 'block';
+				document.getElementById(div).style.display = 'none';
+				}
+
+
+function hideSpotify(target, div){
+				document.getElementById(target).style.display = 'none';
+				document.getElementById(div).style.display = 'block';
+				}
